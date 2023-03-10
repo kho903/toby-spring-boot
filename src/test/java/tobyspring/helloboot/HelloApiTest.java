91b5f902274a43e6626f47366b15bd6a95bbd211
Kim Ji Hun <gmldnr2222@naver.com>
2023-03-10 14:46:22 +0900

    test: add unit test
    - HelloServiceTest
    - HelloControllerTest
    - HelloApiTest fail test

--- src/test/java/tobyspring/helloboot/HelloApiTest.java
+++ src/test/java/tobyspring/helloboot/HelloApiTest.java
@@ -2,7 +2,6 @@ package tobyspring.helloboot;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpHeaders;
@@ -25,5 +24,17 @@ public class HelloApiTest {
 		assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
 		// body Hello Spring
 		assertThat(res.getBody()).isEqualTo("Hello Spring");
+	}
+
+	@Test
+	void failsHelloApi() {
+		// http localhost:8080/hello?name=Spring
+		TestRestTemplate rest = new TestRestTemplate();
+
+		ResponseEntity<String> res =
+			rest.getForEntity("http://localhost:8080/hello?name=", String.class);
+
+		// status 500
+		assertThat(res.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
---
+++ src/test/java/tobyspring/helloboot/HelloControllerTest.java
@@ -0,0 +1,29 @@
+package tobyspring.helloboot;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloControllerTest {
+
+	@Test
+	public void helloController() {
+		HelloController helloController = new HelloController(name -> name);
+
+		String ret = helloController.hello("Test");
+		assertThat(ret).isEqualTo("Test");
+	}
+
+	@Test
+	public void failsHelloController() {
+		HelloController helloController = new HelloController(name -> name);
+
+		assertThatThrownBy(() -> {
+			helloController.hello(null);
+		}).isInstanceOf(IllegalArgumentException.class);
+
+		assertThatThrownBy(() -> {
+			helloController.hello("");
+		}).isInstanceOf(IllegalArgumentException.class);
+	}
+}
