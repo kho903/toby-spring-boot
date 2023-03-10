41821ac424cc17c260a79b62f7f2d78e5d7012d4
Kim Ji Hun <gmldnr2222@naver.com>
2023-03-10 13:28:33 +0900

    feat: Bean의 생명주기 메소드

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -2,6 +2,9 @@ package tobyspring.helloboot;
 
 import java.util.Objects;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,9 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
 	private final HelloService helloService;
+	private final ApplicationContext applicationContext;
 
-	public HelloController(HelloService helloService) {
+	public HelloController(HelloService helloService, ApplicationContext applicationContext) {
 		this.helloService = helloService;
+		this.applicationContext = applicationContext;
+		System.out.println(applicationContext);
 	}
 
 	@GetMapping("/hello")
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,6 +3,7 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -11,17 +12,30 @@ import org.springframework.web.servlet.DispatcherServlet;
 @Configuration
 @ComponentScan
 public class HellobootApplication {
+
+	@Bean
+	public ServletWebServerFactory servletWebServerFactory() {
+		return new TomcatServletWebServerFactory();
+	}
+
+	@Bean
+	public DispatcherServlet dispatcherServlet() {
+		return new DispatcherServlet();
+	}
+
 	public static void main(String[] args) {
 		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
 			@Override
 			protected void onRefresh() {
 				super.onRefresh();
 
-				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+				// dispatcherServlet.setApplicationContext(this); // 스프링 컨테이너가 자동으로 주입해줌.
+
 				WebServer webServer = serverFactory.getWebServer(servletContext -> {
-					servletContext.addServlet("dispatcherServlet",
-						new DispatcherServlet(this)
-					).addMapping("/*"); // 모든 URI를 다 처리할 수 있도록 변경
+					servletContext.addServlet("dispatcherServlet", dispatcherServlet)
+						.addMapping("/*"); // 모든 URI를 다 처리할 수 있도록 변경
 				});
 				webServer.start();
 			}
