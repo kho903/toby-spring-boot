ce4c84b2feb7840adb8f9440be5e0eda7fc1e589
Kim Ji Hun <gmldnr2222@naver.com>
2023-03-14 16:13:53 +0900

    refactor: @TrueConditional, @FalseConditional -> @BooleanConditional
    - add BooleanCondtion

--- src/test/java/tobyspring/study/ConditionalTest.java
+++ src/test/java/tobyspring/study/ConditionalTest.java
@@ -6,6 +6,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
@@ -39,14 +40,13 @@ public class ConditionalTest {
 
 	@Retention(RetentionPolicy.RUNTIME)
 	@Target(ElementType.TYPE)
-	@Conditional(TrueCondition.class)
-	@interface TrueConditional {
-
+	@Conditional(BooleanCondition.class)
+	@interface BooleanConditional {
+		boolean value();
 	}
 
-
 	@Configuration
-	@TrueConditional
+	@BooleanConditional(true)
 	static class Config1 {
 		@Bean
 		MyBean myBean() {
@@ -55,23 +55,8 @@ public class ConditionalTest {
 
 	}
 
-	static class TrueCondition implements Condition {
-
-		@Override
-		public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-			return true;
-		}
-	}
-
-	@Retention(RetentionPolicy.RUNTIME)
-	@Target(ElementType.TYPE)
-	@Conditional(FalseCondition.class)
-	@interface FalseConditional {
-
-	}
-
 	@Configuration
-	@FalseConditional
+	@BooleanConditional(false)
 	static class Config2 {
 		@Bean
 		MyBean myBean() {
@@ -79,12 +64,17 @@ public class ConditionalTest {
 		}
 	}
 
-	static class FalseCondition implements Condition {
+	static class BooleanCondition implements Condition {
+
 		@Override
 		public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-			return false;
+			Map<String, Object> annotationAttributes = metadata.getAnnotationAttributes(
+				BooleanConditional.class.getName());
+			Boolean value = (Boolean)annotationAttributes.get("value");
+			return value;
 		}
 	}
 
-	static class MyBean {}
+	static class MyBean {
+	}
 }
