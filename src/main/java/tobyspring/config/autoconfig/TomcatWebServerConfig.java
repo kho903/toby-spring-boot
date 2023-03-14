3bc882711c222a88a13d6eca28a6655ed833fa17
Kim Ji Hun <gmldnr2222@naver.com>
2023-03-14 15:38:23 +0900

    feat: @Conditional 과 Condition
    - Condition 을 구현한 내부 클래스를 활용해 웹 서버 선택하기

--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -3,10 +3,15 @@ package tobyspring.config.autoconfig;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
+@Conditional(TomcatWebServerConfig.TomcatCondition.class)
 public class TomcatWebServerConfig {
 
 	@Bean("tomcatWebServerFactory")
@@ -14,4 +19,10 @@ public class TomcatWebServerConfig {
 		return new TomcatServletWebServerFactory();
 	}
 
+	static class TomcatCondition implements Condition {
+		@Override
+		public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+			return false;
+		}
+	}
 }
