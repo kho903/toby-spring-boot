474b6284eca6b58312243fd5c2991693cd5e1d33
Kim Ji Hun <gmldnr2222@naver.com>
2023-03-17 16:52:55 +0900

    feat: refactor ServerProperties to Binder

--- src/main/java/tobyspring/config/autoconfig/ServerPropertiesConfig.java
+++ src/main/java/tobyspring/config/autoconfig/ServerPropertiesConfig.java
@@ -1,5 +1,6 @@
 package tobyspring.config.autoconfig;
 
+import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
@@ -10,12 +11,7 @@ public class ServerPropertiesConfig {
 
 	@Bean
 	public ServerProperties serverProperties(Environment environment) {
-		ServerProperties properties = new ServerProperties();
-
-		properties.setContextPath(environment.getProperty("contextPath"));
-		properties.setPort(Integer.parseInt(environment.getProperty("port")));
-
-		return properties;
+		return Binder.get(environment).bind("", ServerProperties.class).get();
 	}
 
 }
