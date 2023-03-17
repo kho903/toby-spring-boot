4b03a3342f98650931519894db2b16f29544bb78
Kim Ji Hun <gmldnr2222@naver.com>
2023-03-17 16:47:31 +0900

    feat: ServerPropertiesConfig 파일 분리

--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,11 +1,9 @@
 package tobyspring.config.autoconfig;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 import tobyspring.config.ConditionalMyOnClass;
 import tobyspring.config.MyAutoConfiguration;
@@ -23,16 +21,6 @@ public class TomcatWebServerConfig {
 		factory.setPort(properties.getPort());
 
 		return factory;
-	}
-
-	@Bean
-	public ServerProperties serverProperties(Environment environment) {
-		ServerProperties properties = new ServerProperties();
-
-		properties.setContextPath(environment.getProperty("contextPath"));
-		properties.setPort(Integer.parseInt(environment.getProperty("port")));
-
-		return properties;
 	}
 
 }
