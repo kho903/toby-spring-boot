9b8d129cc0e8950020f2f4f0d99f4cc305877f0f
Kim Ji Hun <gmldnr2222@naver.com>
2023-03-17 16:36:42 +0900

    feat: add @Valie port default

--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 import tobyspring.config.ConditionalMyOnClass;
 import tobyspring.config.MyAutoConfiguration;
@@ -14,15 +13,20 @@ import tobyspring.config.MyAutoConfiguration;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
 
-	@Value("${contextPath}")
+	@Value("${contextPath:}")
 	String contextPath;
+
+	@Value("${port:8080}")
+	int port;
 
 	@Bean("tomcatWebServerFactory")
 	@ConditionalOnMissingBean
 	public ServletWebServerFactory servletWebServerFactory() {
 		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-		System.out.println("this.contextPath = " + this.contextPath);
+
 		factory.setContextPath(this.contextPath);
+		factory.setPort(port);
+
 		return factory;
 	}
 
