540e42cc17c0cb4395ab61e49f7dbd24554252ca
Kim Ji Hun <gmldnr2222@naver.com>
2023-03-17 16:43:29 +0900

    feat: ServerProperties 클래스 분리

--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 import tobyspring.config.ConditionalMyOnClass;
 import tobyspring.config.MyAutoConfiguration;
@@ -13,21 +14,25 @@ import tobyspring.config.MyAutoConfiguration;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
 
-	@Value("${contextPath:}")
-	String contextPath;
-
-	@Value("${port:8080}")
-	int port;
-
 	@Bean("tomcatWebServerFactory")
 	@ConditionalOnMissingBean
-	public ServletWebServerFactory servletWebServerFactory() {
+	public ServletWebServerFactory servletWebServerFactory(ServerProperties properties) {
 		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
 
-		factory.setContextPath(this.contextPath);
-		factory.setPort(port);
+		factory.setContextPath(properties.getContextPath());
+		factory.setPort(properties.getPort());
 
 		return factory;
+	}
+
+	@Bean
+	public ServerProperties serverProperties(Environment environment) {
+		ServerProperties properties = new ServerProperties();
+
+		properties.setContextPath(environment.getProperty("contextPath"));
+		properties.setPort(Integer.parseInt(environment.getProperty("port")));
+
+		return properties;
 	}
 
 }
